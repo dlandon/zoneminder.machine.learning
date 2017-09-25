31cdba16d92eeb22cb2dd3c8aa52a745ddb1d5c7
dlandon <dlandon@cinci.rr.com>
2017-09-25 06:03:31 -0400

    Add set time script

--- init/30_firstrun.sh
+++ init/30_firstrun.sh
@@ -131,16 +131,6 @@ if [ `stat -c '%a' /var/cache/zoneminder/events/` != '777' ]; then
 	chmod -R go+rw /var/cache/zoneminder
 fi
 
-# Get docker env timezone and set system timezone
-if [[ $(cat /etc/timezone) != "$TZ" ]] ; then
-	echo "Setting the timezone to : $TZ"
-	echo $TZ > /etc/timezone
-	ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
-	dpkg-reconfigure tzdata
-	echo "Date: `date`"
-	sed -i "s|^date.timezone =.*$|date.timezone = ${TZ}|" /etc/php/7.0/apache2/php.ini
-fi
-
 # Fix memory issue
 echo "Setting shared memory to : $SHMEM of `awk '/MemTotal/ {print $2}' /proc/meminfo` bytes"
 umount /dev/shm
