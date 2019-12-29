dc9c6782dfe4c7d31c0684ac487d921a5e4fe260
dlandon <dlandon@cinci.rr.com>
2019-12-29 07:01:48 -0500

    Change default ZM_PATH_FFMPEG.

--- defaults/mysql_defaults.sql
+++ defaults/mysql_defaults.sql
@@ -1,5 +1,5 @@
 # Set up some defaults
-update zm.Config SET Value='/usr/bin/avconv' WHERE Name='ZM_PATH_FFMPEG';
+update zm.Config SET Value='/usr/bin/ffmpeg' WHERE Name='ZM_PATH_FFMPEG';
 update zm.Config SET Value=1 WHERE Name='ZM_OPT_FFMPEG';
 update zm.Config SET Value='-vcodec libx264 -threads 2 -b 2000k -minrate 800k -maxrate 5000k' WHERE Name='ZM_FFMPEG_OUTPUT_OPTIONS';
 update zm.Config SET Value='mp4* mpg mpeg wmv asf avi mov swf 3gp**' WHERE Name='ZM_FFMPEG_FORMATS';
